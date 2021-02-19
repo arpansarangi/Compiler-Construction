2f618c9f282400eff74db08494af7525e256f5f3
arpansarangi <arpansarangi@users.noreply.github.com>
2021-02-19 18:38:25 +0530

    minor fixes, error handling, get delimiters

--- lexer.cpp
+++ lexer.cpp
@@ -7,25 +7,28 @@
 
 using namespace std; 
 
-const string keywords[] = {"main", "void", "boolean", "int", "float", "string", "while", "if", "and", "or", "return", "in", "out"};
+const string keywords[] = {"main", "void", "boolean", "int", "float", "while", "if", "and", "or", "return", "in", "out"};
 
 void error(char ch){
   cout << "Lexical Error: \"" << ch << "\" is not a part of the character set of the language, skipping..." << endl;
 }
 
-void error(string lexeme, string line, int *pos){
-  cout << "Lexical Error: \"" << lexeme << "\" did not match any regular expression, skipping to the next whitespace..." << endl;
-  while(line[(*pos)] and line[(*pos)]!=' ')  (*pos)++;
+void error(string lexeme, int *pos){
+  cout << "Lexical Error: \"" << lexeme << "\" did not match any regular expression, skipping to the next character..." << endl;
+  (*pos)++;
 }
 
 void getComment(string line, int *pos, int lineNumber){
-  cout << "handling comment" << endl;
+  // cout << "handling comment" << endl;
   while(line[(*pos)]) (*pos)++;
 }
 
 void getArithmeticOperator(string line, int *pos, int lineNumber){
   if(line[(*pos)] == '+') {
-    printToken(newToken(101, "+", lineNumber));
+    printToken(newToken(111, "+", lineNumber));
+  }
+  if(line[(*pos)] == '*') {
+    printToken(newToken(112, "*", lineNumber));
   }
   (*pos)++;
 }
@@ -48,101 +51,118 @@ void getRelationalOperator(string line, int *pos, int lineNumber){
 }
 
 void getName(string line, int *pos, int lineNumber){
-  cout << "handling name   ";
+  cout << "~";
   (*pos)++;
 }
 
 void getNumber(string s, int *i, int line_no){
-    cout << "handling number" << endl;
-    int state = 0;
-    string cur_lexeme = "";
-    int flag = 0;
-    if(s[*i] == '-')
+  // cout << "handling number" << endl;
+  int state = 0;
+  string cur_lexeme = "";
+  int flag = 0;
+  if(s[*i] == '-')
+  {
+    cur_lexeme += s[*i];
+    (*i)++;
+    flag = 1;
+  }
+  if(s[*i] < '0' || s[*i] > '9')
+  {
+    state = 404;
+    error(cur_lexeme, i);
+    (*i)--;
+    return;
+    (*i)++;
+  }
+  if(state == 0)
+  {
+    //cout << "a " << s[*i] << "\n";
+    if(s[*i] == '0')
+      state = 1;
+    else 
+      state = 2;
+    cur_lexeme += s[*i];
+    (*i)++;
+  }
+  if(state == 1)
+  {
+    //cout << "b "<< s[*i] << "\n";
+    if(s[*i] == '.')
     {
-        cur_lexeme += s[*i];
-        (*i)++;
-        flag = 1;
+      cur_lexeme += s[*i];
+      (*i)++;
+      state = 3; 
     }
-    if(s[*i] < '0' || s[*i] > '9')
+    else
+      printToken(newToken(102, cur_lexeme, line_no));
+  }
+  if(state == 2)
+  {
+    //cout << "c " << s[*i] << "\n";
+    while(s[*i] >= '0' && s[*i] <= '9')
+      cur_lexeme += s[*i], (*i)++;
+    if(s[*i] == '.')
     {
-        state = 404;
-        (*i)++;
+      cur_lexeme += s[*i];
+      (*i)++;
+      state = 3;
     }
-    if(state == 0)
+    else
+      printToken(newToken(102, cur_lexeme, line_no));
+  }
+  if(state == 3)
+  {
+    if(s[*i] < '0' || s[*i] > '9'){
+      state = 404;
+      error(cur_lexeme, i);
+      (*i)--;
+      return;
+    }
+    else
     {
-        //cout << "a " << s[*i] << "\n";
-        if(s[*i] == '0')
-            state = 1;
-        else 
-            state = 2;
-        cur_lexeme += s[*i];
-        (*i)++;
+      state = 4; 
+      cur_lexeme += s[*i];
+      (*i)++;
     }
-    if(state == 1)
-    {
-        //cout << "b "<< s[*i] << "\n";
-        if(s[*i] == '.')
-        {
-            cur_lexeme += s[*i];
-            (*i)++;
-            state = 3; 
-        }
-        else
-            printToken(newToken(102, cur_lexeme, line_no)), state = 404;
-    }
-    if(state == 2)
-    {
-        //cout << "c " << s[*i] << "\n";
-        while(s[*i] >= '0' && s[*i] <= '9')
-            cur_lexeme += s[*i], (*i)++;
-        if(s[*i] == '.')
-        {
-            cur_lexeme += s[*i];
-            (*i)++;
-            state = 3;
-        }
-        else
-            printToken(newToken(102, cur_lexeme, line_no)), state = 404;
-    }
-    if(state == 3)
-    {
-        if(s[*i] < '0' || s[*i] > '9')
-            state = 404;
-        else
-        {
-            state = 4; 
-            cur_lexeme += s[*i];
-            (*i)++;
-        }
-
-    }
-    if(state == 4)
-    {
-        while(s[*i] >= '0' && s[*i] <= '9')
-            cur_lexeme += s[*i], (*i)++;
-        printToken(newToken(103, cur_lexeme, line_no)), state = 404;
-    }
-}
-
-void getString(string line, int *pos, int lineNumber){
-  cout << "handling string" << endl;
-  (*pos)++;
+  }
+  if(state == 4)
+  {
+    while(s[*i] >= '0' && s[*i] <= '9')
+      cur_lexeme += s[*i], (*i)++;
+    printToken(newToken(103, cur_lexeme, line_no));
+  }
 }
 
 void getAssignmentOperator(string line, int *pos, int lineNumber){
   printToken(newToken(120, ":=", lineNumber));
   (*pos)++;
+  (*pos)++;
 }
 
 void getDelimiter(string line, int *pos, int lineNumber){
-  cout << "handling delimiter" << endl;
+  // cout << "handling delimiter" << endl;
+  if(line[(*pos)] == '{') {
+    printToken(newToken(131, "{", lineNumber));
+  }
+  if(line[(*pos)] == '}') {
+    printToken(newToken(132, "}", lineNumber));
+  }
+  if(line[(*pos)] == '(') {
+    printToken(newToken(133, "(", lineNumber));
+  }
+  if(line[(*pos)] == ')') {
+    printToken(newToken(134, ")", lineNumber));
+  }
+  if(line[(*pos)] == ',') {
+    printToken(newToken(135, ",", lineNumber));
+  }
   (*pos)++;
 }
 
 int main(){
   ifstream fin;
-  fin.open("tests/1in.txt");
-  freopen("tests/1out-lexer.txt","w",stdout);
+  fin.open("tests/3in.txt");
+  freopen("tests/3out-lexer.txt","w",stdout);
   string line;
   int lineNumber = 0;
   while (getline(fin, line)) { 
@@ -150,7 +170,7 @@ int main(){
     cout << "line" << lineNumber << endl;
     int pos = 0;
     while(line[pos]){
-      if(line[pos]<='9' and line[pos]>='0'){
+      if((line[pos]<='9' and line[pos]>='0') or line[pos]=='-'){
         getNumber(line, &pos, lineNumber);
       }
       else if((line[pos]>='a' and line[pos]<='z') or (line[pos]>='A' and line[pos]<='Z')){
@@ -167,12 +187,12 @@ int main(){
           case '>':
           case '=':  call(getRelationalOperator);
           case ':':  call(getAssignmentOperator);
-          case '"':  call(getString);
           case '(':
           case ')':
           case '{':
           case '}':
           case ',':  call(getDelimiter);
+          case '.': error(".", &pos); break;
           //some other cases
           default:   error(line[pos]); pos++; break;  //error - character is invalid
         }
